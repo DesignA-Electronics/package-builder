19650eb99447e0287e6d828c19145cd4c29afb3d
andre <andre@f6b31dc6-dd17-0410-bcf4-f92f679a180a>
2010-10-13 21:10:36 +0000

    Do md5sum & size in packages
    git-svn-id: file:///work/internal/svn/package_builder@1599 f6b31dc6-dd17-0410-bcf4-f92f679a180a

--- update_packages_list.sh
+++ update_packages_list.sh
@@ -11,6 +11,8 @@ for f in packages/*.ipk ; do
     echo -n "."
     ar p $f control.tar.gz | tar xzO control >> $OUTPUT
     echo "Filename: `basename $f`" >> $OUTPUT
+    echo "MD5Sum: `md5sum $f | cut -d' ' -f 1`" >> $OUTPUT
+    echo "Size: `wc -c $f | cut -d' ' -f 1`" >> $OUTPUT
     echo >> $OUTPUT
 done
 
