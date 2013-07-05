d9ae52221d4d3b5e941b0d73a84ff18f70953122
Andre Renaud <andre@bluewatersys.com>
2013-07-05 15:19:58 +1200

    Added in c++ compiler

--- package_builder.cmake
+++ package_builder.cmake
@@ -4,4 +4,5 @@ include(CMakeForceCompiler)
 # Prefix detection only works with compiler id "GNU"
 # CMake will look for prefixed g++, cpp, ld, etc. automatically
 CMAKE_FORCE_C_COMPILER(arm-none-linux-gnueabi-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(arm-none-linux-gnueabi-g++ GNU)
 
